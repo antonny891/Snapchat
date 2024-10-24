8e923623e5955654002af8fc6df211612fd89b06
antonny <melber.mamanchura@tecsup.edu.pe>
2024-10-23 20:53:58 -0500

    no message

---
+++ MamanchuraAlccaSnapchat/SnapsViewController.swift
@@ -0,0 +1,34 @@
+//
+//  SnapsViewController.swift
+//  MamanchuraAlccaSnapchat
+//
+//  Created by Antonny Mmamanchura Alcca on 23/10/24.
+//
+
+import UIKit
+
+class SnapsViewController: UIViewController {
+
+    @IBAction func cerrarSesionTapped(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
